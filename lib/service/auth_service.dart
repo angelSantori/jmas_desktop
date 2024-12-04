8a0518d20953d9e3f40732d1f0009850ac1d036a
angelSantori <antonyo.da.mm@gmail.com>
2024-12-04 09:02:10 -0700

    Operaciones con el token

--- lib/service/auth_service.dart
+++ lib/service/auth_service.dart
@@ -1,3 +1,23 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
 class AuthService {
   final String apiURL = 'https://localhost:7048/api';
+
+  //Save token en almacenamiento local
+  Future<void> saveToken(String token) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('authToken', token);
+  }
+
+  //Obtener token del almacenamiento local
+  Future<String?> getToken() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString('authToken');
+  }
+
+  //Delete token logout
+  Future<void> deleteToken() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.remove('authToken');
+  }
 }
