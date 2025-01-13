a1b8f38df53a863aed40513ac835505276ef5dd9
angelSantori <antonyo.da.mm@gmail.com>
2025-01-12 18:52:49 -0700

    Ajuste menos

---
+++ lib/ajustes_minus/add_ajuste_menos_page.dart
@@ -0,0 +1,291 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:jmas_desktop/contollers/ajuste_menos_controller.dart';
+import 'package:jmas_desktop/contollers/productos_controller.dart';
+import 'package:jmas_desktop/service/auth_service.dart';
+import 'package:jmas_desktop/widgets/componentes.dart';
+import 'package:jmas_desktop/widgets/mensajes.dart';
+
+class AddAjusteMenosPage extends StatefulWidget {
+  const AddAjusteMenosPage({super.key});
+
+  @override
+  State<AddAjusteMenosPage> createState() => _AddAjusteMenosPageState();
+}
+
+class _AddAjusteMenosPageState extends State<AddAjusteMenosPage> {
+  final AuthService _authService = AuthService();
+  final AjusteMenosController _ajusteMenosController = AjusteMenosController();
+  final ProductosController _productosController = ProductosController();
+
+  final TextEditingController _cantidadController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+  final TextEditingController _idProductoController = TextEditingController();
+
+  final String _fecha = DateFormat('dd/MM/yyyy').format(DateTime.now());
+  final _formKey = GlobalKey<FormState>();
+
+  final List<Map<String, dynamic>> _productosAgregados = [];
+
+  bool _isLoading = false;
+
+  String? idUserReporte;
+
+  Productos? _selectedProducto;
+
+  void _agregarProducto() {
+    if (_selectedProducto != null && _cantidadController.text.isNotEmpty) {
+      final int cantidad = int.tryParse(_cantidadController.text) ?? 0;
+
+      if (cantidad <= 0) {
+        showAdvertence(context, 'La cantidad debe ser mayor a 0.');
+        return;
+      }
+
+      setState(() {
+        final double precioUnitario =
+            _selectedProducto!.producto_Precio1 ?? 0.0;
+        final double precioTotal = precioUnitario * cantidad;
+
+        _productosAgregados.add({
+          'id': _selectedProducto!.id_Producto,
+          'descripcion': _selectedProducto!.producto_Descripcion,
+          'costo': _selectedProducto!.producto_Precio1,
+          'cantidad': cantidad,
+          'precio': precioTotal
+        });
+
+        //Limpiar campos despuués de agregar
+        _idProductoController.clear();
+        _cantidadController.clear();
+        _selectedProducto = null;
+      });
+    } else {
+      showAdvertence(
+          context, 'Debe seleccionar un producto y definir la cantidad.');
+    }
+  }
+
+  Future<void> _guardarAjusteMenos() async {
+    if (_productosAgregados.isEmpty) {
+      showAdvertence(context, 'Debe agregar al menos un producto');
+      return;
+    }
+
+    if (_formKey.currentState!.validate()) {
+      bool success = true;
+      for (var producto in _productosAgregados) {
+        await _getUserId();
+        final nuevoAjusteMenos = _crearAjusteMenos(producto);
+        bool result =
+            await _ajusteMenosController.addAjusteMenos(nuevoAjusteMenos);
+
+        if (!result) {
+          success = false;
+          break;
+        }
+
+        if (producto['id'] == null) {
+          showAdvertence(context,
+              'Id nulo: ${producto['id_Producto']}, no se puede continuar.');
+          success = false;
+          break;
+        }
+
+        final productoActualizado =
+            await _productosController.getProductoById(producto['id']);
+
+        if (productoActualizado == null) {
+          showAdvertence(context,
+              'Producto con ID: ${producto['id']} no encontrado en la base de datos.');
+          success = false;
+          break;
+        }
+
+        productoActualizado.producto_Existencia =
+            (productoActualizado.producto_Existencia!) - producto['cantidad'];
+
+        bool editResult =
+            await _productosController.editProducto(productoActualizado);
+
+        if (!editResult) {
+          showAdvertence(context,
+              'Error al actualizar las existencias del producto con ID: ${producto['id_Producto']}');
+          success = false;
+          break;
+        }
+      }
+
+      // Mostrar el mensaje correspondiente al finalizar el ciclo
+      if (success) {
+        // ignore: use_build_context_synchronously
+        showOk(context, 'Ajuste Más creado exitosamente.');
+      } else {
+        // ignore: use_build_context_synchronously
+        showError(context, 'Error al registrar ajuste más');
+      }
+
+      _limpiarFormulario();
+    }
+  }
+
+  Future<void> _getUserId() async {
+    final decodeToken = await _authService.decodeToken();
+    idUserReporte = decodeToken?['Id_User'] ?? '0';
+  }
+
+  AjusteMenos _crearAjusteMenos(Map<String, dynamic> producto) {
+    return AjusteMenos(
+        id_AjusteMenos: 0,
+        ajusteMenos_Descripcion: _descriptionController.text,
+        ajusteMenos_Cantidad: double.parse(producto['cantidad'].toString()),
+        ajusteMenos_Fecha: _fecha,
+        id_Producto: producto['id'],
+        id_User: int.parse(idUserReporte ?? '0'));
+  }
+
+  void _limpiarFormulario() {
+    _formKey.currentState!.reset();
+    _productosAgregados.clear();
+    setState(() {
+      _descriptionController.clear();
+      _cantidadController.clear();
+      _idProductoController.clear();
+      _productosAgregados.clear();
+      _selectedProducto = null;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Ajuste Menos'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 50),
+          child: Form(
+            key: _formKey,
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  //Fecha
+                  Text(
+                    _fecha,
+                    style: const TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+
+                  //Descripción
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      buildFormRow(
+                        label: 'Descripción',
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: TextFormField(
+                                controller: _descriptionController,
+                                decoration: const InputDecoration(
+                                  labelText: 'Descripción',
+                                  border: OutlineInputBorder(),
+                                ),
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'La descripción no puede estar vacía';
+                                  }
+                                  return null;
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+
+                  //Buscar producto
+                  BuscarProductoWidget(
+                    idProductoController: _idProductoController,
+                    cantidadController: _cantidadController,
+                    productosController: _productosController,
+                    isLoading: _isLoading,
+                    selectedProducto: _selectedProducto,
+                    onProductoSeleccionado: (producto) {
+                      setState(() {
+                        _selectedProducto = producto;
+                      });
+                    },
+                    onAdvertencia: (p0) {
+                      showAdvertence(context, p0);
+                    },
+                  ),
+
+                  const SizedBox(height: 20),
+
+                  //Botón
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      ElevatedButton.icon(
+                        onPressed: _agregarProducto,
+                        icon: const Icon(
+                          Icons.add,
+                          color: Colors.white,
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blue.shade900,
+                        ),
+                        label: const Text(
+                          'Agregar',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  const SizedBox(height: 20),
+
+                  buildProductosAgregados(_productosAgregados),
+
+                  const SizedBox(height: 20),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: _guardarAjusteMenos,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blue.shade900,
+                        ),
+                        child: const Text(
+                          'Guardar Ajuste -',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  const SizedBox(height: 20),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/contollers/ajuste_menos_controller.dart
@@ -0,0 +1,169 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/io_client.dart';
+
+import 'package:jmas_desktop/service/auth_service.dart';
+
+class AjusteMenosController {
+  final AuthService _authService = AuthService();
+
+  IOClient _createHttpClient() {
+    final ioClient = HttpClient();
+    ioClient.badCertificateCallback =
+        (X509Certificate cert, String host, int port) => true;
+    return IOClient(ioClient);
+  }
+
+  Future<List<AjusteMenos>> listAjusteMenos() async {
+    final IOClient client = _createHttpClient();
+    try {
+      final response = await client.get(
+        Uri.parse('${_authService.apiURL}/AjustesMenos'),
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final List<dynamic> jsonData = json.decode(response.body);
+        return jsonData
+            .map((ajusteMenos) => AjusteMenos.fromMap(ajusteMenos))
+            .toList();
+      } else {
+        return [];
+      }
+    } catch (e) {
+      // ignore: avoid_print
+      print('Error lista ajuste menos: $e');
+      return [];
+    }
+  }
+
+  Future<bool> addAjusteMenos(AjusteMenos ajusteMenos) async {
+    final IOClient client = _createHttpClient();
+    try {
+      final response = await client.post(
+        Uri.parse('${_authService.apiURL}/AjustesMenos'),
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: ajusteMenos.toJson(),
+      );
+
+      if (response.statusCode == 201) {
+        return true;
+      } else {
+        // ignore: avoid_print
+        print(
+            'Error al realizar ajusteMenos: ${response.statusCode} - ${response.body}');
+        return false;
+      }
+    } catch (e) {
+      // ignore: avoid_print
+      print('Error al crear ajusteMenos: $e');
+      return false;
+    }
+  }
+}
+
+class AjusteMenos {
+  int? id_AjusteMenos;
+  String? ajusteMenos_Descripcion;
+  double? ajusteMenos_Cantidad;
+  String? ajusteMenos_Fecha;
+  int? id_Producto;
+  int? id_User;
+  AjusteMenos({
+    this.id_AjusteMenos,
+    this.ajusteMenos_Descripcion,
+    this.ajusteMenos_Cantidad,
+    this.ajusteMenos_Fecha,
+    this.id_Producto,
+    this.id_User,
+  });
+
+  AjusteMenos copyWith({
+    int? id_AjusteMenos,
+    String? ajusteMenos_Descripcion,            
+    double? ajusteMenos_Cantidad,
+    String? ajusteMenos_Fecha,
+    int? id_Producto,
+    int? id_User,
+  }) {
+    return AjusteMenos(
+      id_AjusteMenos: id_AjusteMenos ?? this.id_AjusteMenos,
+      ajusteMenos_Descripcion:
+          ajusteMenos_Descripcion ?? this.ajusteMenos_Descripcion,
+      ajusteMenos_Cantidad: ajusteMenos_Cantidad ?? this.ajusteMenos_Cantidad,
+      ajusteMenos_Fecha: ajusteMenos_Fecha ?? this.ajusteMenos_Fecha,
+      id_Producto: id_Producto ?? this.id_Producto,
+      id_User: id_User ?? this.id_User,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id_AjusteMenos': id_AjusteMenos,
+      'ajusteMenos_Descripcion': ajusteMenos_Descripcion,
+      'ajusteMenos_Cantidad': ajusteMenos_Cantidad,
+      'ajusteMenos_Fecha': ajusteMenos_Fecha,
+      'id_Producto': id_Producto,
+      'id_User': id_User,
+    };
+  }
+
+  factory AjusteMenos.fromMap(Map<String, dynamic> map) {
+    return AjusteMenos(
+      id_AjusteMenos:
+          map['id_AjusteMenos'] != null ? map['id_AjusteMenos'] as int : null,
+      ajusteMenos_Descripcion: map['ajusteMenos_Descripcion'] != null
+          ? map['ajusteMenos_Descripcion'] as String
+          : null,
+      ajusteMenos_Cantidad: map['ajusteMenos_Cantidad'] != null
+          ? (map['ajusteMenos_Cantidad'] is int
+              ? (map['ajusteMenos_Cantidad'] as int).toDouble()
+              : map['ajusteMenos_Cantidad'] as double)
+          : null,
+      ajusteMenos_Fecha: map['ajusteMenos_Fecha'] != null
+          ? map['ajusteMenos_Fecha'] as String
+          : null,
+      id_Producto:
+          map['id_Producto'] != null ? map['id_Producto'] as int : null,
+      id_User: map['id_User'] != null ? map['id_User'] as int : null,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory AjusteMenos.fromJson(String source) =>
+      AjusteMenos.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'AjusteMenos(id_AjusteMenos: $id_AjusteMenos, ajusteMenos_Descripcion: $ajusteMenos_Descripcion, ajusteMenos_Cantidad: $ajusteMenos_Cantidad, ajusteMenos_Fecha: $ajusteMenos_Fecha, id_Producto: $id_Producto, id_User: $id_User)';
+  }
+
+  @override
+  bool operator ==(covariant AjusteMenos other) {
+    if (identical(this, other)) return true;
+
+    return other.id_AjusteMenos == id_AjusteMenos &&
+        other.ajusteMenos_Descripcion == ajusteMenos_Descripcion &&
+        other.ajusteMenos_Cantidad == ajusteMenos_Cantidad &&
+        other.ajusteMenos_Fecha == ajusteMenos_Fecha &&
+        other.id_Producto == id_Producto &&
+        other.id_User == id_User;
+  }
+
+  @override
+  int get hashCode {
+    return id_AjusteMenos.hashCode ^
+        ajusteMenos_Descripcion.hashCode ^
+        ajusteMenos_Cantidad.hashCode ^
+        ajusteMenos_Fecha.hashCode ^
+        id_Producto.hashCode ^
+        id_User.hashCode;
+  }
+}
