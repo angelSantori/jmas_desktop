ac7daa34cc0559d884d0a5fa9818719cf074d9bd
angelSantori <antonyo.da.mm@gmail.com>
2025-01-13 23:04:07 -0700

    Mejor lista de usuarios

--- lib/users/list_user_page.dart
+++ lib/users/list_user_page.dart
@@ -18,109 +18,125 @@ class _ListUserPageState extends State<ListUserPage> {
       appBar: AppBar(
         title: const Text('Lista de usuarios'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(left: 60, right: 60),
-        child: FutureBuilder<List<Users>>(
-          future: _usersController.listUsers(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            } else if (snapshot.hasError) {
-              return Center(
-                child: Text('Error: ${snapshot.error}'),
-              );
-            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-              return const Center(
-                child: Text('No hay usuarios registrados'),
-              );
-            }
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: FutureBuilder<List<Users>>(
+            future: _usersController.listUsers(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return Center(
+                  child: Text('Error: ${snapshot.error}'),
+                );
+              } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                return const Center(
+                  child: Text('No hay usuarios registrados'),
+                );
+              }
 
-            final users = snapshot.data!;
+              final users = snapshot.data!;
 
-            return ListView.builder(
-              itemCount: users.length,
-              itemBuilder: (context, index) {
-                final user = users[index];
-                return Card(
-                  color: Colors.blue.shade900,
-                  margin:
-                      const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                  elevation: 4,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12)),
-                  child: Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              //Nombre
-                              Text(
-                                '${user.user_Name}',
-                                style: const TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              const SizedBox(height: 10),
-
-                              //Contacto
-                              Text(
-                                '${user.user_Contacto}',
-                                style: const TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 18,
-                                ),
-                              ),
-                              const SizedBox(height: 10),
-
-                              //Nombre
-                              Text(
-                                'Palabra de acceso: ${user.user_Access}',
-                                style: const TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 15,
-                                ),
-                              ),
-                              const SizedBox(height: 10),
-                            ],
-                          ),
+              return Column(
+                children: [
+                  Flexible(
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 50),
+                      child: GridView.builder(
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2, //Mostrar de 2 en 2
+                          crossAxisSpacing: 20, //Espacio horizontal
+                          mainAxisSpacing: 30, //Espacio vertical
+                          childAspectRatio: 4, //Ancho y alto de las tarjetas
                         ),
-                        Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            IconButton(
-                              icon: const Icon(
-                                Icons.edit,
-                                color: Colors.white,
-                                size: 30,
-                              ),
-                              onPressed: () async {
-                                final result = await Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) =>
-                                        EditUserPage(user: user),
+                        itemCount: users.length,
+                        itemBuilder: (context, index) {
+                          final user = users[index];
+                          return Card(
+                            color: const Color.fromARGB(255, 201, 230, 242),
+                            elevation: 10,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15)),
+                            child: Padding(
+                              padding: const EdgeInsets.all(12),
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Expanded(
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        //Nombre
+                                        Text(
+                                          '${user.user_Name}',
+                                          style: const TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        const SizedBox(height: 5),
+
+                                        //Contacto
+                                        Text(
+                                          '${user.user_Contacto}',
+                                          style: const TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 18,
+                                          ),
+                                        ),
+                                        const SizedBox(height: 5),
+
+                                        //Nombre
+                                        Text(
+                                          'Palabra de acceso: ${user.user_Access}',
+                                          style: const TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 15,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
                                   ),
-                                );
-                                if (result == true) {
-                                  setState(() {});
-                                }
-                              },
-                            )
-                          ],
-                        )
-                      ],
+                                  Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      IconButton(
+                                        icon: const Icon(
+                                          Icons.edit,
+                                          color: Colors.black,
+                                          size: 20,
+                                        ),
+                                        onPressed: () async {
+                                          final result = await Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                              builder: (context) =>
+                                                  EditUserPage(user: user),
+                                            ),
+                                          );
+                                          if (result == true) {
+                                            setState(() {});
+                                          }
+                                        },
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      ),
                     ),
                   ),
-                );
-              },
-            );
-          },
+                ],
+              );
+            },
+          ),
         ),
       ),
     );
