7471c77c0ad0abd6d93fe75c5ab230e719d0231b
angelSantori <antonyo.da.mm@gmail.com>
2025-01-24 14:02:18 -0700

    Archivo widgets para salida

---
+++ lib/widgets/widgets_salida.dart
@@ -0,0 +1,502 @@
+import 'dart:convert';
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+import 'package:jmas_desktop/contollers/productos_controller.dart';
+import 'package:jmas_desktop/widgets/formularios.dart';
+import 'package:jmas_desktop/widgets/mensajes.dart';
+
+class BuscarProductoWidgetSalida extends StatefulWidget {
+  final TextEditingController idProductoController;
+  final TextEditingController cantidadController;
+  final ProductosController productosController;
+  final Productos? selectedProducto;
+  final Function(Productos?) onProductoSeleccionado;
+  final Function(String) onAdvertencia;
+
+  const BuscarProductoWidgetSalida({
+    Key? key,
+    required this.idProductoController,
+    required this.cantidadController,
+    required this.productosController,
+    required this.selectedProducto,
+    required this.onProductoSeleccionado,
+    required this.onAdvertencia,
+  }) : super(key: key);
+
+  @override
+  State<BuscarProductoWidgetSalida> createState() =>
+      _BuscarProductoWidgetStateSalida();
+}
+
+class _BuscarProductoWidgetStateSalida
+    extends State<BuscarProductoWidgetSalida> {
+  final ValueNotifier<bool> _isLoading = ValueNotifier(false);
+  final ValueNotifier<double?> _precioAjustado = ValueNotifier(null);
+
+  Uint8List? _decodeImage(String? base64Image) {
+    if (base64Image == null || base64Image.isEmpty) {
+      return null;
+    }
+    try {
+      return base64Decode(base64Image);
+    } catch (e) {
+      return null;
+    }
+  }
+
+  Future<void> _buscarProducto() async {
+    final id = widget.idProductoController.text;
+    if (id.isNotEmpty) {
+      widget.onProductoSeleccionado(null);
+      _isLoading.value = true;
+
+      try {
+        final producto =
+            await widget.productosController.getProductoById(int.parse(id));
+        if (producto != null) {
+          widget.onProductoSeleccionado(producto);
+          _precioAjustado.value = producto.prodPrecio;
+        } else {
+          widget.onAdvertencia('Producto con ID: $id, no encontrado');
+        }
+      } catch (e) {
+        widget.onAdvertencia('Error al buscar el producto: $e');
+      } finally {
+        _isLoading.value = false; // Finalizar el estado de carga
+      }
+    } else {
+      widget.onAdvertencia('Por favor, ingrese un ID de producto.');
+    }
+  }
+
+  void _ajustarPrecio(double porcentaje) {
+    if (widget.selectedProducto?.prodPrecio != null) {
+      final precioBase = widget.selectedProducto!.prodPrecio!;
+      _precioAjustado.value = precioBase + (precioBase * porcentaje / 100);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        // Campo para ID del Producto
+        SizedBox(
+          width: 160,
+          child: CustomTextFielTexto(
+            controller: widget.idProductoController,
+            prefixIcon: Icons.search,
+            labelText: 'Id Producto',
+          ),
+        ),
+        const SizedBox(width: 15),
+
+        // Botón para buscar producto
+        ValueListenableBuilder<bool>(
+          valueListenable: _isLoading,
+          builder: (context, isLoading, child) {
+            return ElevatedButton(
+              onPressed: isLoading ? null : _buscarProducto,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: isLoading
+                    ? Colors.grey
+                    : Colors.blue.shade900, // Cambiar color si está cargando
+              ),
+              child: isLoading
+                  ? const SizedBox(
+                      height: 16,
+                      width: 16,
+                      child: CircularProgressIndicator(
+                        strokeWidth: 2,
+                        color: Colors.white,
+                      ),
+                    )
+                  : const Text(
+                      'Buscar producto',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+            );
+          },
+        ),
+        const SizedBox(width: 15),
+
+        // Información del Producto
+        if (widget.selectedProducto != null)
+          Expanded(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Información del Producto:',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 5),
+                      Text(
+                        'Descripción: ${widget.selectedProducto!.prodDescripcion ?? 'No disponible'}',
+                        style: const TextStyle(fontSize: 14),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      Text(
+                        'Existencia: ${widget.selectedProducto!.prodExistencia ?? 'No disponible'}',
+                        style: const TextStyle(fontSize: 14),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      Text(
+                        'Precio: \$${widget.selectedProducto!.prodPrecio?.toStringAsFixed(2) ?? 'No disponible'}',
+                        style: const TextStyle(fontSize: 14),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      ValueListenableBuilder<double?>(
+                        valueListenable: _precioAjustado,
+                        builder: (context, precio, child) {
+                          return Text(
+                            'Precio ajustado: \$${precio?.toStringAsFixed(2) ?? 'No disponible'}',
+                            style: const TextStyle(fontSize: 14),
+                            overflow: TextOverflow.ellipsis,
+                          );
+                        },
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Column(
+                            children: [
+                              IconButton(
+                                icon: const Icon(Icons.arrow_upward),
+                                color: Colors.green,
+                                onPressed: () => _ajustarPrecio(10),
+                              ),
+                              const Text('10%', style: TextStyle(fontSize: 12)),
+                            ],
+                          ),
+                          const SizedBox(width: 10),
+                          Column(
+                            children: [
+                              IconButton(
+                                icon: const Icon(Icons.horizontal_rule),
+                                color: Colors.orange,
+                                onPressed: () => _ajustarPrecio(5),
+                              ),
+                              const Text('5%', style: TextStyle(fontSize: 12)),
+                            ],
+                          ),
+                          const SizedBox(width: 10),
+                          Column(
+                            children: [
+                              IconButton(
+                                icon: const Icon(Icons.arrow_downward),
+                                color: Colors.red,
+                                onPressed: () => _ajustarPrecio(0),
+                              ),
+                              const Text('0%', style: TextStyle(fontSize: 12)),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 150,
+                        width: 150,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                          color: Colors.grey.shade200,
+                        ),
+                        child: widget.selectedProducto!.prodImgB64 != null
+                            ? Image.memory(
+                                _decodeImage(
+                                    widget.selectedProducto!.prodImgB64)!,
+                                fit: BoxFit.cover,
+                              )
+                            : Image.asset(
+                                'assets/images/sinFoto.jpg',
+                                fit: BoxFit.cover,
+                              ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )
+        else
+          const Expanded(
+            flex: 2,
+            child: Text(
+              'No se ha buscado un producto.',
+              style: TextStyle(fontSize: 14, fontStyle: FontStyle.italic),
+            ),
+          ),
+        const SizedBox(width: 10),
+
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Campo para la cantidad
+            SizedBox(
+              width: 140,
+              child: CustomTextFieldNumero(
+                controller: widget.cantidadController,
+                prefixIcon: Icons.numbers_outlined,
+                labelText: 'Cantidad',
+              ),
+            ),
+            const SizedBox(height: 10),
+
+            //Botón tabla
+            ElevatedButton(
+              onPressed: () {
+                final cantidadText = widget.cantidadController.text;
+                if (widget.selectedProducto != null &&
+                    cantidadText.isNotEmpty) {
+                  final cantidad = int.parse(cantidadText);
+                  if (cantidad != null && cantidad > 0) {
+                    _agregarProducto(widget.selectedProducto!, cantidad);
+                  } else {
+                    showAdvertence(context, 'Ingrese una cantidad válida.');
+                  }
+                } else {
+                  showAdvertence(
+                      context, 'Seleccione un producto y cantidad válida.');
+                }
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green.shade700,
+              ),
+              child: const Text(
+                'Agregar',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  final List<Map<String, dynamic>> productosAgregados = [];
+
+  void _agregarProducto(Productos producto, int cantidad) {
+    final precioIncrementado =
+        _precioAjustado.value ?? producto.prodPrecio ?? 0.0;
+    final precioTotal = precioIncrementado * cantidad;
+
+    final productoAgregado = { 
+      'id': producto.id_Producto,
+      'descripcion': producto.prodDescripcion ?? 'Sin descripción',
+      'costo': producto.prodPrecio ?? 0.0,
+      'cantidad': cantidad,
+      'porcentaje': ((_precioAjustado.value ?? producto.prodPrecio ?? 0.0) -
+              (producto.prodPrecio ?? 0.0)) /
+          (producto.prodPrecio ?? 1) *
+          100,
+      'precioIncrementado': precioIncrementado,
+      'precio': precioTotal,
+    };
+
+    // Agrega el producto a la lista
+    setState(() {
+      productosAgregados.add(productoAgregado);
+    });
+  }
+}
+
+//Tabla de productos
+Widget buildProductosAgregadosSalida(
+    List<Map<String, dynamic>> productosAgregados) {
+  if (productosAgregados.isEmpty) {
+    return const Text(
+      'No hay productos agregados.',
+      style: TextStyle(fontStyle: FontStyle.italic),
+    );
+  }
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      const Text(
+        'Productos Agregados:',
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+      ),
+      const SizedBox(height: 10),
+      Table(
+        border: TableBorder.all(),
+        columnWidths: const {
+          0: FlexColumnWidth(1), //ID
+          1: FlexColumnWidth(3), //Descripción
+          2: FlexColumnWidth(1), //Costo
+          3: FlexColumnWidth(1), //Cantidad
+          4: FlexColumnWidth(1), //% Incremento
+          5: FlexColumnWidth(1), //Precio incrementado
+          6: FlexColumnWidth(1), //Precio total
+        },
+        children: [
+          TableRow(
+            decoration: BoxDecoration(
+              color: Colors.blue.shade900,
+            ),
+            children: const [
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'Clave',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.white),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'Descripción',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.white),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'Costo',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.white),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'Cantidad',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.white),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  '% Incremento',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.white),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'Precio incrementado',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.white),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'Precio Total',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.white),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          ),
+          ...productosAgregados.map((producto) {
+            return TableRow(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    producto['id'].toString(),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    producto['descripcion'] ?? 'Sin descripción',
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 1,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    '\$${(producto['costo'] ?? 0.0).toStringAsFixed(2)}',
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    (producto['cantidad'] ?? 00).toString(),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    '${(producto['porcentaje'] ?? 0.0).toStringAsFixed(2)}%',
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    '\$${(producto['precioIncrementado'] ?? 0.0).toStringAsFixed(2)}',
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    '\$${(producto['precio'] ?? 0.0).toStringAsFixed(2)}',
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              ],
+            );
+          }),
+          TableRow(children: [
+            const Padding(padding: EdgeInsets.all(8.0), child: Text('')),
+            const Padding(padding: EdgeInsets.all(8.0), child: Text('')),
+            const Padding(padding: EdgeInsets.all(8.0), child: Text('')),
+            const Padding(padding: EdgeInsets.all(8.0), child: Text('')),
+            const Padding(padding: EdgeInsets.all(8.0), child: Text('')),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                'Total',
+                style: TextStyle(fontWeight: FontWeight.bold),
+                textAlign: TextAlign.right,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                '\$${productosAgregados.fold<double>(0.0, (previousValue, producto) => previousValue + (producto['precio'] ?? 0.0)).toStringAsFixed(2)}',
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+          ])
+        ],
+      ),
+    ],
+  );
+}
