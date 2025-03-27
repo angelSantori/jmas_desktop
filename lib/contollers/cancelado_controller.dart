b3c0cdd71994dca319eb97ad5e59b2beb88e3e44
angelSantori <antonyo.da.mm@gmail.com>
2025-03-26 22:37:20 -0700

    X

--- lib/contollers/cancelado_controller.dart
+++ lib/contollers/cancelado_controller.dart
@@ -1,10 +1,9 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:jmas_desktop/service/auth_service.dart';
 
 class CanceladoController {
-  AuthService _authService = AuthService();
+  final AuthService _authService = AuthService();
 
   Future<bool> addCancelacion(Cancelados cancelado) async {
     try {
