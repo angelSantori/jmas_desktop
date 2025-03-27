d9212cc6d96305eebe7bde02adcc3e0e373c949c
angelSantori <antonyo.da.mm@gmail.com>
2025-03-26 22:35:42 -0700

    Cancelado Salida -Controller -Lista cancelados

---
+++ lib/contollers/cancelado_salida_controller.dart
@@ -0,0 +1,141 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:jmas_desktop/service/auth_service.dart';
+
+class CanceladoSalidaController {
+  final AuthService _authService = AuthService();
+
+  Future<bool> addCancelSalida(CanceladoSalidas cancelSalida) async {
+    try {
+      final response = await http.post(
+        Uri.parse('${_authService.apiURL}/CanceladoSalidas'),
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: cancelSalida.toJson(),
+      );
+
+      if (response.statusCode == 201) {
+        return true;
+      } else {
+        print(
+            'Error al agregar cancelación salida | Ife | Controller: ${response.statusCode} - ${response.body}');
+        return false;
+      }
+    } catch (e) {
+      print('Error al agregar canclación Salida | TryCatch | Controller: $e');
+      return false;
+    }
+  }
+
+  Future<List<CanceladoSalidas>> listCanceladoSalida() async {
+    try {
+      final response = await http
+          .get(Uri.parse('${_authService.apiURL}/CanceladoSalidas'), headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+      });
+
+      if (response.statusCode == 200) {
+        final List<dynamic> jsonData = json.decode(response.body);
+        return jsonData
+            .map((canceladoSalida) => CanceladoSalidas.fromMap(canceladoSalida))
+            .toList();
+      } else {
+        print(
+            'Error al obtener lista cancelaciones salida | Ife | Controller: ${response.statusCode} - ${response.body}');
+        return [];
+      }
+    } catch (e) {
+      print(
+          'Error al obtener lista cancelaciones salida | TryCatch | Controller: $e');
+      return [];
+    }
+  }
+}
+
+class CanceladoSalidas {
+  int? idCanceladoSalida;
+  String? cancelSalidaMotivo;
+  String? cancelSalidaFecha;
+  int? id_Salida;
+  int? id_User;
+  CanceladoSalidas({
+    this.idCanceladoSalida,
+    this.cancelSalidaMotivo,
+    this.cancelSalidaFecha,
+    this.id_Salida,
+    this.id_User,
+  });
+
+  CanceladoSalidas copyWith({
+    int? idCanceladoSalida,
+    String? cancelSalidaMotivo,
+    String? cancelSalidaFecha,
+    int? id_Salida,
+    int? id_User,
+  }) {
+    return CanceladoSalidas(
+      idCanceladoSalida: idCanceladoSalida ?? this.idCanceladoSalida,
+      cancelSalidaMotivo: cancelSalidaMotivo ?? this.cancelSalidaMotivo,
+      cancelSalidaFecha: cancelSalidaFecha ?? this.cancelSalidaFecha,
+      id_Salida: id_Salida ?? this.id_Salida,
+      id_User: id_User ?? this.id_User,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'idCanceladoSalida': idCanceladoSalida,
+      'cancelSalidaMotivo': cancelSalidaMotivo,
+      'cancelSalidaFecha': cancelSalidaFecha,
+      'id_Salida': id_Salida,
+      'id_User': id_User,
+    };
+  }
+
+  factory CanceladoSalidas.fromMap(Map<String, dynamic> map) {
+    return CanceladoSalidas(
+      idCanceladoSalida: map['idCanceladoSalida'] != null
+          ? map['idCanceladoSalida'] as int
+          : null,
+      cancelSalidaMotivo: map['cancelSalidaMotivo'] != null
+          ? map['cancelSalidaMotivo'] as String
+          : null,
+      cancelSalidaFecha: map['cancelSalidaFecha'] != null
+          ? map['cancelSalidaFecha'] as String
+          : null,
+      id_Salida: map['id_Salida'] != null ? map['id_Salida'] as int : null,
+      id_User: map['id_User'] != null ? map['id_User'] as int : null,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory CanceladoSalidas.fromJson(String source) =>
+      CanceladoSalidas.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'CanceladoSalidas(idCanceladoSalida: $idCanceladoSalida, cancelSalidaMotivo: $cancelSalidaMotivo, cancelSalidaFecha: $cancelSalidaFecha, id_Salida: $id_Salida, id_User: $id_User)';
+  }
+
+  @override
+  bool operator ==(covariant CanceladoSalidas other) {
+    if (identical(this, other)) return true;
+
+    return other.idCanceladoSalida == idCanceladoSalida &&
+        other.cancelSalidaMotivo == cancelSalidaMotivo &&
+        other.cancelSalidaFecha == cancelSalidaFecha &&
+        other.id_Salida == id_Salida &&
+        other.id_User == id_User;
+  }
+
+  @override
+  int get hashCode {
+    return idCanceladoSalida.hashCode ^
+        cancelSalidaMotivo.hashCode ^
+        cancelSalidaFecha.hashCode ^
+        id_Salida.hashCode ^
+        id_User.hashCode;
+  }
+}
---
+++ lib/salidas/list_cancelacioens_salida_page.dart
@@ -0,0 +1,212 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:jmas_desktop/contollers/cancelado_salida_controller.dart';
+import 'package:jmas_desktop/contollers/salidas_controller.dart';
+import 'package:jmas_desktop/contollers/users_controller.dart';
+import 'package:jmas_desktop/widgets/componentes.dart';
+
+class ListCancelacioensSalidaPage extends StatefulWidget {
+  const ListCancelacioensSalidaPage({super.key});
+
+  @override
+  State<ListCancelacioensSalidaPage> createState() =>
+      _ListCancelacioensSalidaPageState();
+}
+
+class _ListCancelacioensSalidaPageState
+    extends State<ListCancelacioensSalidaPage> {
+  final CanceladoSalidaController _canceladoSalidaController =
+      CanceladoSalidaController();
+  final UsersController _usersController = UsersController();
+  final SalidasController _salidasController = SalidasController();
+
+  List<CanceladoSalidas> _allCanceladosSalida = [];
+  List<CanceladoSalidas> _filteredCanceladosSalida = [];
+
+  DateTime? _startDate;
+  DateTime? _endDate;
+
+  Map<int, Salidas> _salidasCache = {};
+  Map<int, Users> _usersCache = {};
+
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+  }
+
+  Future<void> _loadData() async {
+    try {
+      final cancelados = await _canceladoSalidaController.listCanceladoSalida();
+      final salidas = await _salidasController.listSalidas();
+      final users = await _usersController.listUsers();
+
+      setState(() {
+        _allCanceladosSalida = cancelados;
+        _filteredCanceladosSalida = cancelados;
+
+        _salidasCache = {for (var sali in salidas) sali.id_Salida!: sali};
+        _usersCache = {for (var use in users) use.id_User!: use};
+
+        _isLoading = false;
+      });
+    } catch (e) {
+      print('Error al cargar datos | ListCancelacionesSalida: $e');
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  void _filterCanceladosSalida() {
+    setState(() {
+      _filteredCanceladosSalida = _allCanceladosSalida.where(
+        (canceladoSalida) {
+          final fechaString = canceladoSalida.cancelSalidaFecha;
+
+          final fecha = fechaString != null ? parseDate(fechaString) : null;
+
+          final matchesDate = fecha != null &&
+              (_startDate == null || !fecha.isBefore(_startDate!)) &&
+              (_endDate == null || !fecha.isAfter(_endDate!));
+
+          return matchesDate;
+        },
+      ).toList();
+    });
+  }
+
+  Future<void> _selectDateRange(BuildContext context) async {
+    final picked = await showDateRangePicker(
+      context: context,
+      firstDate: DateTime(2000),
+      lastDate: DateTime.now(),
+      initialDateRange: _startDate != null && _endDate != null
+          ? DateTimeRange(start: _startDate!, end: _endDate!)
+          : null,
+    );
+
+    if (picked != null) {
+      setState(() {
+        _startDate = picked.start;
+        _endDate = picked.end;
+      });
+      _filterCanceladosSalida();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _isLoading
+          ? Center(
+              child: CircularProgressIndicator(color: Colors.blue.shade900))
+          : Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Row(
+                    children: [
+                      Expanded(
+                          child: ElevatedButton.icon(
+                        onPressed: () => _selectDateRange(context),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              const Color.fromARGB(255, 201, 230, 242),
+                        ),
+                        icon: const Icon(Icons.calendar_today,
+                            color: Colors.black),
+                        label: Text(
+                          _startDate != null && _endDate != null
+                              ? 'Desde: ${DateFormat('yyyy-MM-dd').format(_startDate!)} Hasta: ${DateFormat('yyyy-MM-dd').format(_endDate!)}'
+                              : 'Seleccionar rango de fechas',
+                          style: const TextStyle(
+                            color: Colors.black,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                      )),
+                      IconButton(
+                        icon: const Icon(Icons.clear, color: Colors.black),
+                        onPressed: () {
+                          setState(() {
+                            _startDate = null;
+                            _endDate = null;
+                            _filterCanceladosSalida();
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: _buildListView(),
+                )
+              ],
+            ),
+    );
+  }
+
+  Widget _buildListView() {
+    return _filteredCanceladosSalida.isEmpty
+        ? const Center(
+            child: Text(
+              'No hay cancelaciones diponibles',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          )
+        : ListView.builder(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            itemCount: _filteredCanceladosSalida.length,
+            itemBuilder: (context, index) {
+              final canceladoSalida = _filteredCanceladosSalida[index];
+              final salida = _salidasCache[canceladoSalida.id_Salida];
+              final user = _usersCache[canceladoSalida.id_User];
+
+              return Card(
+                margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                color: const Color.fromARGB(255, 201, 230, 242),
+                child: ListTile(
+                  title: Text(
+                    'Id cancelación: ${canceladoSalida.idCanceladoSalida}',
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(height: 10),
+                      Text(
+                        'Realizado por: ${user?.user_Name ?? 'Usuario no encontrado'}',
+                        style: const TextStyle(fontSize: 15),
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                        'Fecha: ${canceladoSalida.cancelSalidaFecha ?? 'Fecha no disponible'}',
+                        style: const TextStyle(fontSize: 15),
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                        'Motivo: ${canceladoSalida.cancelSalidaMotivo ?? 'Motivo no encontrado'}',
+                        style: const TextStyle(fontSize: 15),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      const SizedBox(height: 10),
+                      if (salida != null)
+                        Text(
+                          'Id de Salida: ${salida.id_Salida} - Folio: ${salida.salida_CodFolio}',
+                          style: const TextStyle(fontSize: 15),
+                        ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+  }
+}
